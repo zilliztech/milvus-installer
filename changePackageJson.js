6c5440fdbaea896b099c1dc0eaa12e172830850e
chenzizhao <nameczz@126.com>
2020-09-27 17:46:19 +0800

    fix

--- changePackageJson.js
+++ changePackageJson.js
@@ -9,6 +9,8 @@ function writeJson(name) {
     let packageJson = data.toString(); //将二进制的数据转换为字符串
     packageJson = JSON.parse(packageJson); //将字符串转换为json对象
     packageJson.build.appId = name;
+    packageJson.name = name;
+
     const str = JSON.stringify(packageJson); //因为nodejs的写入文件只认识字符串或者二进制数，所以把json对象转换成字符串重新写入json文件中
     fs.writeFile("./package.json", str, function (err) {
       if (err) {
